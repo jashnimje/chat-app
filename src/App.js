cd35d0589d25b8c603656e5ed5e9bb929c9a1bcc
Jash Nimje <jashnimje@gmail.com>
2021-08-15 16:09:46 +0530

    Google Auth and Login Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // BEM Naming System
 function App() {
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
   return (
     <div className="app">
-      <div className="app__body">
-        <Router >
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId" >
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div >
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router >
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId" >
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div >
+      )}
     </div >
   );
 }
